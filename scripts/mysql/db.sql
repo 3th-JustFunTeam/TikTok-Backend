34e38413093c7667e12d07eda17b0b1586ec7004
zsy-arch <helloworld1915@outlook.com>
2022-05-18 23:58:23 +0800

    update: mysql

--- scripts/mysql/db.sql
+++ scripts/mysql/db.sql
@@ -1,4 +1,5 @@
-create database if not exists tiktok;
+drop database if exists tiktok;
+create database tiktok;
 
 use tiktok;
 
@@ -19,8 +20,8 @@ drop table if exists Follows;
 create table Follows
 (
     id           bigint unsigned auto_increment not null,
-    user_id      int                            not null comment '被关注者ID',
-    following_id int                            not null comment '关注者ID',
+    user_id      bigint unsigned                not null comment '被关注者ID',
+    following_id bigint unsigned                not null comment '关注者ID',
     ts           timestamp                      not null,
     is_deleted   bool                           not null,
     primary key (`id`),
@@ -32,7 +33,7 @@ drop table if exists Videos;
 create table Videos
 (
     id         bigint unsigned auto_increment not null,
-    author_id  int                            not null,
+    author_id  bigint unsigned                not null,
     ts         timestamp                      not null,
     update_ts  timestamp                      not null,
     is_deleted bool                           not null,
@@ -44,7 +45,7 @@ drop table if exists VideosData;
 create table VideosData
 (
     id          bigint unsigned auto_increment not null,
-    video_id    int                            not null,
+    video_id    bigint unsigned                not null,
     title       nvarchar(25)                   not null,
     description nvarchar(50)                   not null,
     video_path  varchar(64)                    not null,
@@ -56,8 +57,8 @@ drop table if exists VideoComments;
 create table VideoComments
 (
     id         bigint unsigned auto_increment not null,
-    user_id    int                            not null,
-    video_id   int                            not null,
+    user_id    bigint unsigned                not null,
+    video_id   bigint unsigned                not null,
     ts         timestamp                      not null,
     update_ts  timestamp                      not null,
     is_deleted bool                           not null,
@@ -70,7 +71,7 @@ drop table if exists VideoCommentsData;
 create table VideoCommentsData
 (
     id               bigint unsigned auto_increment not null,
-    video_comment_id int                            not null,
+    video_comment_id bigint unsigned                not null,
     comment_message  nvarchar(100)                  not null,
     primary key (`id`),
     foreign key (`video_comment_id`) references `VideoComments` (`id`) on delete cascade on update cascade
@@ -80,8 +81,8 @@ drop table if exists Favorites;
 create table Favorites
 (
     id         bigint unsigned auto_increment not null,
-    video_id   int                            not null,
-    user_id    int                            not null,
+    video_id   bigint unsigned                not null,
+    user_id    bigint unsigned                not null,
     ts         timestamp                      not null,
     update_ts  timestamp                      not null,
     is_deleted bool                           not null,
