05f70fa2b24e024e889b7607513f2b0c508d882b
yang <3167292824@qq.com>
2022-06-12 12:32:10 +0800

    投稿视频 调整代码

--- service/videoService.go
+++ service/videoService.go
@@ -102,17 +102,10 @@ func GetUserInfoByIds(ids []int, token string) ([]vo.UserInfo, error) {
 }
 
 // AddVideo 添加视频
-func AddVideo(token, title, fileName string) error {
-
-	// 解析token
-	claims, err2 := utils.ParseToken(token)
-
-	if err2 != nil {
-		return errors.New("token 解析错误")
-	}
+func AddVideo(userId int, title, fileName string) error {
 
 	video := po.Video{
-		UserId:      uint(claims.UserId),
+		UserId:      uint(userId),
 		PlayUrl:     fileName,
 		CoverUrl:    common.DEFAULT_COVERURL,
 		Title:       title,
